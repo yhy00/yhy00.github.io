253c49f2bd46b075acd6737b26adaf53bf7f3276
yhy00 <65645793+yhy00@users.noreply.github.com>
2021-10-08 17:31:03 +0800

    Add files via upload

---
+++ szxy/php/bd.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <?php include "func.php"; ?> -->
+</head>
+<body>
+    <?php
+    each $_POST["name"];
+    br();
+    echo $_POST["num"];
+    br();
+    echo $_POST["qus"];
+    ?>
+</body>
+</html>
